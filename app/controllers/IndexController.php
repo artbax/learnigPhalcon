9549dd68b22db829be59acc92f9701893471e27d
artbax <artbaxcpp@gmail.com>
2018-01-13 15:27:25 +0100

    my second commit

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,8 +5,13 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        return "To jest tekst z kontrolera w Phalcon ";
+        return "To jest zmiana wprowadzona w kontroler";
     }
+	
+	public function showAction()
+	{
+		return "a to jakis tekst, ktory powinien sie gdzies pokazac...";
+	}
 
 }
 
